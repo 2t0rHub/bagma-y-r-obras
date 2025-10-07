28d3365d77fb7abbd55e6ba593e6401c80033080
2t0rHub <soyel2t0r@gmail.com>
2025-10-07 23:35:09 +0200

    Final test for the font problem

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,10 +5,8 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", ...fontFamily.sans],
-      },
+    fontFamily: {
+      sans: ["Inter", ...fontFamily.sans],
     },
   },
   plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
